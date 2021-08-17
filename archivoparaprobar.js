221c605ea04458696edc6f4a4d6f5502fd924d08
Julio González <gonzaulio@gmail.com>
2021-08-17 02:22:09 +0200

    Subida de cambios de mi segunda rama

--- archivoparaprobar.js
+++ archivoparaprobar.js
@@ -2,3 +2,6 @@ let valor1 = 2;
 let valor2 = 3;
 let resultado = (valor1 + valor2);
 console.log(resultado);
+
+let resultadomultiplicacion = (valor1 * valor2);
+console.log(resultadomultiplicacion);
